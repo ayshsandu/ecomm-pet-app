a7bc643e791fc407977108fdf4b071fce14f682a
Ayesha Dissanayaka <aysh@ayesha.local>
2023-02-26 22:48:00 +0530

    Implement a temp cart storage in local storage

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ import Admin from './components/Admin/Admin.js';
 // Main app component
 const App = () => {
   // Define cart and add to cart variables to pass to the MyCart component
+  // const [userId, setUserId] = useState();
   const [cart, setCart] = useState([]);
+  const { state, signIn, signOut } = useAuthContext();
 
   // Component to render the login/signup/logout menu
   const RightLoginSignupMenu = () => {
-    const { state, signIn, signOut } = useAuthContext();
     const signUpURL = "https://accounts.asgardeo.io/t/ayeshaecomm/accountrecoveryendpoint/register.do?client_id="
       + process.env.REACT_APP_CLIENT_ID + "&sp=" + process.env.REACT_APP_APPLICATION_NAME;
 
@@ -26,19 +27,18 @@ const App = () => {
 
     // Host the menu content and return it at the end of the function
     let menu;
-
     // Conditionally render the Mycart and Admin links based on whether the user is logged in or not
     if (isLoggedIn) {
       if (state.allowedScopes.includes(process.env.REACT_APP_ADD_ITEMS_SCOPE)) {
         menu = (
           <Menu.Item position="right">
             <Menu.Item as={Link} to="/admin" name="Admin" />
-            <Menu.Item as={Link} to="/mycart" name="My Cart" />
+            <Menu.Item as={Link} to="/mycart" content={`Cart (${cart.length})`} icon="cart" />
             <Menu.Item>
               <FontAwesomeIcon icon={faUser} />
               {state.username ? state.username : ""}
             </Menu.Item>
-            <Button primary onClick={() => signOut()}>
+            <Button primary onClick={handleSignOut()}>
               Logout
             </Button>
           </Menu.Item>
@@ -46,7 +46,7 @@ const App = () => {
       } else {
         menu = (
           <Menu.Item position="right">
-            <Menu.Item as={Link} to="/mycart" name="My Cart" content={`Cart (${cart.length})`} icon="cart" />
+            <Menu.Item as={Link} to="/mycart" content={`Cart (${cart.length})`} icon="cart" />
             <Menu.Item>
               <FontAwesomeIcon icon={faUser} />
               {state.username ? state.username : ""}
@@ -74,6 +74,11 @@ const App = () => {
       );
     }
     return menu;
+
+    function handleSignOut() {
+      localStorage.removeItem('cart.');
+      return () => signOut();
+    }
   }
 
   // Component to render the navigation bar
@@ -97,17 +102,23 @@ const App = () => {
   };
 
   const addToCart = item => {
-    setCart([...cart, item]);
+    const newCart = [...cart, item];
+    setCart(newCart);
+    localStorage.setItem('cart.', JSON.stringify(newCart));
   };
 
   const removeFromCart = itemId => {
     var newCart = cart.filter(item => item.id !== itemId);
     setCart(newCart);
+    localStorage.setItem('cart.', JSON.stringify(newCart));
   }
 
 
   useEffect(() => {
     document.title = 'PetStore';
+    if(localStorage.getItem('cart.')){
+      setCart(JSON.parse(localStorage.getItem('cart.')));
+    }
   }, []);
   return (
     <>
@@ -115,7 +126,7 @@ const App = () => {
         <PetStoreNav />
         <Switch>
           <Route exact path="/">
-            <Catalog cart={cart} handleAddToCart={addToCart} handleRemoveFromCart = {removeFromCart} />
+            <Catalog cart={cart} handleAddToCart={addToCart} handleRemoveFromCart={removeFromCart} />
           </Route>
           <Route path="/mycart">
             <MyCart cart={cart} removeFromCart={removeFromCart} />
