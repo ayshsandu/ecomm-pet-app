1fdf7c30c7daf9ed36035467624f29779ac3354a
Ayesha Dissanayaka <aysh@ayesha.local>
2023-05-11 21:27:00 +0530

    Update footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,16 +9,13 @@ function Footer() {
         <Grid divided inverted stackable>
           <Grid.Row>
             <Grid.Column width={10}>
-              <h4>About</h4>
               <p>
-              Welcome to Snout & Paws Shop, the ultimate destination for all your furry friend's needs! 
               At Snout & Paws Shop, we understand the deep bond between humans and their beloved pets. 
               That's why we strive to provide a carefully curated collection of products that cater to their every need and desire. 
               </p>
             </Grid.Column
 >            <Grid.Column width={4}>
-              <h4>Contact us</h4>
-              <p>+94777603866</p>
+              <p>Contact us: +94777603866</p>
             </Grid.Column>
           </Grid.Row>
         </Grid>
